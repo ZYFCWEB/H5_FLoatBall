48fd819c627e8deb1121e128b6ce40ffb61c74fb
ZYFCWEB <290803498@QQ.com>
2016-05-27 11:28:59 +0800

    Create FloatBall.js

---
+++ FloatBall.js
@@ -0,0 +1,91 @@
+/*
+ * Author:      dan.wang && xiangyu.bai
+ * Create date: 2016-05-24
+ * Description: 悬浮窗插件,该脚本用于指游方寸H5游戏 悬浮窗功能的实现接入
+ * 使用方法:
+ * 1.引入js文件
+ * <script src="http://r.3333.cn/a8sdk/PlugIn/FloatBall/js/FloatBall.js" type="text/javascript" ></script>
+ * 2.使用以下方法初始化组件
+    H5_FloatBall.showBall({
+            url: "http://www.3333.cn/",
+            desc: "",//默认分享理由(可选)
+            title: '燃烧吧二弟web',//分享标题(可选)
+            pics: 'http://r.3333.cn/a8sdk/images/h5_rsbed_share_icon.jpg', //分享图片的路径(可选)
+    });
+ */
+
+// 声明悬浮窗对象
+var H5_FloatBall = new Object();
+
+// 声明常用参数
+H5_FloatBall.FloatBallHtml = "";        // 悬浮窗代码
+H5_FloatBall.ShareParams = {            // 分享参数初始
+    url:   "http://www.3333.cn/",
+    desc:  "指游方寸",
+    title: "指游方寸-专注发行,只做精品",
+    pics:  "http://r.3333.cn/3333/gw/pc/images/toplogo.png"
+};
+
+
+/**
+ * 初始化悬浮设置
+ */
+H5_FloatBall.init = function(){
+
+}
+
+/**
+ * 调用显示悬浮窗
+ */
+H5_FloatBall.showBall = function(){
+
+}
+
+/**
+ * 到电脑玩
+ */
+H5_FloatBall.playToPC = function(){
+
+}
+
+/**
+ * 分享功能
+ */
+H5_FloatBall.shareAPI = function(){
+
+}
+
+/**
+ * 保存下载
+ */
+H5_FloatBall.downGame = function(){
+
+}
+
+/**
+ * 退出游戏
+ */
+H5_FloatBall.exitGame = function(){
+
+}
+
+/**
+ * 返回
+ */
+H5_FloatBall.goBack = function(){
+
+}
+
+/**
+ * 转到指游部落
+ */
+H5_FloatBall.goTozybl = function(){
+
+}
+
+/**
+ * 绑定手机
+ */
+H5_FloatBall.bindPhone = function(){
+
+}
